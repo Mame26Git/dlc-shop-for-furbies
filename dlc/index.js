8a2fe61854d02698646c0fcc13b643b9dcddff29
Mame26Git <kaicalpin@gmail.com>
2026-09-19 17:31:55 +0100

    Update DLC configuration for Packs 1 and 2

--- dlc/index.js
+++ dlc/index.js
@@ -1,17 +1,17 @@
 [
-    {
+    { 
         "file": "tu002560.dlc", 
         "title": "Pack 1",
         "buttons": [
-            { "title": "Alert", "action": [75,0,0,0] }
+            { "title": "Logo", "action": [75,0,0,0] }
         ]
     },
-    {
+    { 
         "file": "tu020710.dlc", 
         "title": "Pack 2",
         "buttons": [
-            { "title": "Alert", "action": [75,0,0,0] }
+            { "title": "Action", "action": [75,0,3,4] },
+            { "title": "Action2", "action": [75,0,4,4] }
         ]
-    },
-    
+    }
 ]
