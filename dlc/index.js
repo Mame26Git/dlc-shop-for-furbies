2087660ce75f706dc636349dc64060f3c7826593
Mame26Git <kaicalpin@gmail.com>
2026-09-19 16:43:14 +0100

    New DLC!
    Next one in 22:42.

--- dlc/index.js
+++ dlc/index.js
@@ -5,5 +5,13 @@
         "buttons": [
             { "title": "Alert", "action": [75,0,0,0] }
         ]
-    }
+    },
+    {
+        "file": "tu020710.dlc", 
+        "title": "Pack 2",
+        "buttons": [
+            { "title": "Alert", "action": [75,0,0,0] }
+        ]
+    },
+    
 ]
