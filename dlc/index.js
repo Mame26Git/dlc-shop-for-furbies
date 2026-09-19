3d93cac288dd106172ce1c82ed48c79db17a0a61
Mame26Git <kaicalpin@gmail.com>
2026-09-19 11:42:09 +0100

    Update DLC index with new pack and remove others

--- dlc/index.js
+++ dlc/index.js
@@ -1,51 +1,8 @@
 [
-    { 
+    {
         "file": "context.dlc", 
-        "title": "Context Logo",
+        "title": "Pack 1",
         "buttons": [
-            { "title": "Logo", "action": [75,0,4,4] }
+            { "title": "Alert", "action": [75,0,0,0] }
         ]
-    },
-    { 
-        "file": "hacked.dlc", 
-        "title": "EXTRONDIMATE ALL HUMAEMNS",
-        "buttons": [
-            { "title": "Hacked 1", "action": [75,0,3,4] },
-            { "title": "Hacked 2", "action": [75,0,4,4] }
-        ]
-    },
-    {
-        "file": "noimg2.dlc", 
-        "title": "Audio only",
-        "buttons": [
-            { "title": "Audio test ", "action": [75,0,4,4] }
-        ]
-    },
-    {
-        "file": "chilli.dlc", 
-        "title": "Chilli",
-        "buttons": [
-            { "title": "Chilli ", "action": [75,0,4,4] }
-             ]
-    },
-    {
-        "file": "OLIVER.dlc", 
-        "title": "oliver... does it work???",
-        "buttons": [
-            { "title": "oliver... does it work???", "action": [75,0,4,4] }
-
-        ]
-        },
-    {
-        "file": "fu001680.dlc", 
-        "title": "i still dont know!",
-        "buttons": [
-            { "title": "i still dont know!", "action": [75,0,4,4] }
-
-        ]
-    },    {
-        "file": "Waaagh.dlc", 
-        "title": "Waaagh",
-        "buttons": [
-            { "title": "WAAAGH!!", "action": [75,0,0,2] }
-]
+    }
