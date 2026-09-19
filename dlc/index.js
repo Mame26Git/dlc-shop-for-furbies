a8ea1afc94ef7a9a030e71a2b9d4173de437df88
Mame26Git <kaicalpin@gmail.com>
2026-09-19 18:55:46 +0100

    Update titles and files in DLC configuration

--- dlc/index.js
+++ dlc/index.js
@@ -8,17 +8,17 @@
     },
     { 
         "file": "tu020710.dlc", 
-        "title": "Package 2",
+        "title": "Package 2 is broken",
         "buttons": [
             { "title": "Test 1", "action": [75,0,3,4] },
             { "title": "Test 2", "action": [75,0,4,4] }
         ]
     },
     {
-        "file": "noimg2.dlc", 
-        "title": "Audio only",
+        "file": "tu020510.dlc", 
+        "title": "Package 3",
         "buttons": [
-            { "title": "Audio test ", "action": [75,0,4,4] }
+            { "title": " test ", "action": [75,0,4,4] }
         ]
     },
     {
